008f748018831a13f0711c8bf4b10bee3373fb6b
zxniu <niu_zhongxin@hoperun.com>
2017-12-13 17:00:55 +0800

    git环境测试

--- eureka-sever/src/main/java/com/lovnx/EurekaServer.java
+++ eureka-sever/src/main/java/com/lovnx/EurekaServer.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServer {
-
+	//启动服务
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(EurekaServer.class).web(true).run(args);
 	}
